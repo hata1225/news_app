fb67ec98188d3e292839f79d6c0680dae3d51029
hatapi <y.hata1225@icloud.com>
2021-03-04 13:16:58 +0900

    add ListItem Component

--- App.js
+++ App.js
@@ -1,15 +1,7 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
-
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Hello React Native!!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import { StyleSheet, Text, View, Image } from "react-native";
+import ListItem from "./components/ListItem";
 
 const styles = StyleSheet.create({
   container: {
@@ -18,4 +10,46 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
   },
+  itemContainer: {
+    height: 100,
+    width: "100%",
+    borderColor: "gray",
+    borderWidth: 1,
+    flexDirection: "row",
+  },
+  leftContainer: {
+    width: 100,
+  },
+  rightContainer: {
+    flex: 1,
+    flexDirection: "column",
+    padding: 10,
+    justifyContent: "space-between",
+  },
+  newsImage: {
+    width: 100,
+    height: 100,
+  },
+  text: {
+    fontSize: 16,
+  },
+  subText: {
+    fontSize: 12,
+    color: "gray",
+  },
 });
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <ListItem
+        imageUrl="https://picsum.photos/200/300"
+        title="Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat."
+        author="sampleNews"
+      />
+    </View>
+  );
+}
