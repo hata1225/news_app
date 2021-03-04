16f9934a7c998a4f08b351785221fecc3fc280a9
hatapi <y.hata1225@icloud.com>
2021-03-04 13:58:13 +0900

    add FlatListによるスクロール

--- App.js
+++ App.js
@@ -1,47 +1,19 @@
 import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View, Image } from "react-native";
+import { StyleSheet, View, FlatList } from "react-native";
 import ListItem from "./components/ListItem";
-import article from "./dummies/articles.json";
+import articles from "./dummies/articles.json";
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: "#fff",
-    alignItems: "center",
     justifyContent: "center",
-  },
-  itemContainer: {
-    height: 100,
-    width: "100%",
-    borderColor: "gray",
-    borderWidth: 1,
-    flexDirection: "row",
-  },
-  leftContainer: {
-    width: 100,
-  },
-  rightContainer: {
-    flex: 1,
-    flexDirection: "column",
-    padding: 10,
-    justifyContent: "space-between",
-  },
-  newsImage: {
-    width: 100,
-    height: 100,
-  },
-  text: {
-    fontSize: 16,
-  },
-  subText: {
-    fontSize: 12,
-    color: "gray",
   },
 });
 
 export default function App() {
-  const items = article.map((article, index) => {
+  const items = articles.map((article, index) => {
     // article.jsonを展開、一つ一つの要素が引数articleに入り、returnでListItemoを返したものを変数itemsに入れている。
     // ListItemコンポーネントが複数個itemsに入っているので、keyを設定してreactが追跡できるようにする。
     return (
@@ -54,5 +26,21 @@ export default function App() {
     );
   });
 
-  return <View style={styles.container}>{items}</View>;
+  return (
+    <View style={styles.container}>
+      <FlatList
+        data={articles}
+        // articlesから一つずつ取り出したのがitem
+        renderItem={({ item }) => (
+          <ListItem
+            imageUrl={item.urlToImage}
+            title={item.title}
+            author={item.author}
+          />
+        )}
+        //keyの設定、indexもkeyExtractorから自動で返ってくる
+        keyExtractor={(item, index) => index.toString()}
+      />
+    </View>
+  );
 }
--- components/ListItem.js
+++ components/ListItem.js
@@ -11,6 +11,7 @@ const styles = StyleSheet.create({
   },
   leftContainer: {
     width: 100,
+    height: "100%",
   },
   rightContainer: {
     flex: 1,
